03302281817697ada5c85dc0d85618e73ef514b4
Syscat <60985614+syscat29@users.noreply.github.com>
2025-11-04 00:57:09 -0400

    Add font, base style and header component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,89 @@
-<script setup></script>
+<script setup>
+  import Header from "./components/Header.vue"
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <main>
+    <Header />
+  </main>
 </template>
 
-<style scoped></style>
+<style global>
+  @font-face {
+    font-family: "Geist";
+    font-display: swap;
+    font-style: normal;
+    font-weight: 100 900;
+    src: url("/fonts/Geist.ttf"), format("truetype");
+  }
+
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  h1,
+  h2,
+  h3 {
+    font-weight: inherit;
+    font-size: inherit;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  :root {
+    --bg-primary: #0d0d0d;
+    --bg-secondary: #141414;
+    --text-primary: #f0f0f0;
+    --text-muted: #c7c7c7;
+    --text-faint: #b3b3b3;
+    --text-strong: #ffffff;
+
+    color-scheme: light dark;
+    color: var(--text-muted);
+    background-color: var(--bg-secondary);
+
+    font-synthesis: none;
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  body {
+    font-family: "Geist", system-ui, Avenir, Helvetica, Arial, sans-serif;
+    line-height: 1.5;
+    font-weight: 400;
+    font-size: 15px;
+    min-height: 100dvh;
+    display: flex;
+    justify-content: center;
+  }
+
+  main {
+    width: 100%;
+    max-width: 672px;
+    padding: 3rem 1rem 2rem;
+    display: flex;
+    flex-direction: column;
+    gap: 2.5rem;
+    position: relative;
+  }
+
+  section {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+    font-size: 15px;
+
+    h2 {
+      font-weight: 700;
+      color: var(--text-strong);
+    }
+  }
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,75 @@
+<script setup>
+  import Github from "../icons/Github.vue"
+  import Linkedin from "../icons/Linkedin.vue"
+</script>
+
+<template>
+  <header>
+    <div class="profile">
+      <!-- <img
+        src="https://avatars.githubusercontent.com/u/60985614?v=4"
+        alt="Avatar"
+      /> -->
+      <div>
+        <h1>SYSCAT</h1>
+        <p>Web Developer</p>
+      </div>
+    </div>
+
+    <nav class="socials">
+      <a href="/" target="_blank">
+        <Github size="20" />
+      </a>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+  header {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .profile {
+    display: flex;
+    gap: 1rem;
+    align-items: center;
+
+    img {
+      height: 48px;
+      width: 48px;
+      border-radius: 100%;
+    }
+
+    h1 {
+      color: var(--text-strong);
+      font-weight: 700;
+    }
+
+    p {
+      color: var(--text-muted);
+      font-size: 15px;
+    }
+  }
+
+  .socials {
+    display: flex;
+
+    a {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      padding: 0.5rem;
+      border-radius: 4px;
+      color: var(--text-muted);
+      transition: all 0.3s ease;
+
+      &:hover {
+        background-color: #ffffff15;
+        color: var(--text-strong);
+      }
+    }
+  }
+</style>
