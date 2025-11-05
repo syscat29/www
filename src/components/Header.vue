60a1af067ab8ce4ecd98400324ec3bd969256c72
Syscat <60985614+syscat29@users.noreply.github.com>
2025-11-04 23:16:37 -0400

    chore(header): update css styles

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,8 +8,8 @@
     <div class="profile">
       <!-- <img src="https://avatars.githubusercontent.com/u/60985614?v=4" alt="Avatar" /> -->
       <div>
-        <h1>Syscat</h1>
-        <p>Developer</p>
+        <h1>Angel</h1>
+        <p>@sachellein</p>
       </div>
     </div>
 
@@ -30,46 +30,46 @@
     flex-direction: row;
     justify-content: space-between;
     align-items: center;
-  }
 
-  .profile {
-    display: flex;
-    gap: 1rem;
-    align-items: center;
-
-    img {
-      height: 48px;
-      width: 48px;
-      border-radius: 100%;
-    }
-
-    h1 {
-      color: var(--text-strong);
-      font-weight: 700;
-      font-size: 1.25rem;
-    }
-
-    p {
-      color: var(--text-muted);
-      font-size: 15px;
-    }
-  }
-
-  nav {
-    display: flex;
-
-    a {
+    .profile {
       display: flex;
-      justify-content: center;
+      gap: 1rem;
       align-items: center;
-      padding: 0.5rem;
-      border-radius: 4px;
-      color: var(--text-muted);
-      transition: all 0.3s ease;
 
-      &:hover {
-        background-color: #ffffff15;
+      img {
+        height: 48px;
+        width: 48px;
+        border-radius: 100%;
+      }
+
+      h1 {
         color: var(--text-strong);
+        font-weight: 700;
+        font-size: 1.25rem;
+      }
+
+      p {
+        color: var(--text-muted);
+        font-size: 15px;
+      }
+    }
+
+    nav {
+      display: flex;
+
+      a {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 0.5rem;
+        border-radius: 4px;
+        color: var(--text-muted);
+        transition: all 0.3s ease;
+
+        &:hover {
+          background-color: #ffffff15;
+          color: var(--text-strong);
+        }
       }
     }
   }
